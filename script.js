50a7b69395e1df034965dc0eb2bbde8de2e5e30a
ddavian1 <andresdavidpantoja14@gmail.com>
2024-08-26 21:57:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('redirectButton').addEventListener('click', function() {
+    // Cambia 'pagina-destino.html' por la URL de la página a la que deseas abrir
+    window.open('https://www.youtube.com/watch?v=XSKHA0f_-XA', '_blank'); // Abre en una nueva ventana o pestaña
+});
